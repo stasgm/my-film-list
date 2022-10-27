9bcbc959b9ec6c92258b7354d59751fefdc9ddf5
Stanislau Shlyahtich <stanislau.shlyahtich@sprintingsoftware.com>
2022-10-27 14:13:15 +0300

    Update react-query

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,5 +1,5 @@
-import { QueryClient, QueryClientProvider } from 'react-query'
-// import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// import { ReactQueryDevtools } from '@tanstack/react-query/devtools";
 import FilmList from './Films';
 import Layout from './Layout';
 import { ModalProvider } from './Modal';
--- client/src/hooks/useDefaultQuery.ts
+++ client/src/hooks/useDefaultQuery.ts
@@ -1,5 +1,5 @@
-import type { QueryKey, QueryFunction } from "react-query";
-import { useQuery } from "react-query";
+import type { QueryKey, QueryFunction } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
 import { queryClient } from "../utils";
 
